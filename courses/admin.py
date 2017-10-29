4a1698417f1a2deec1480b3d597eede085e706a7
sahasakkhar <sakkhar@mail.com>
2017-10-29 22:41:19 +0600

    Elearning Platform

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Subject
+from .models import Course
+from .models import Module
+
+admin.site.register(Subject)
+admin.site.register(Course)
+admin.site.register(Module)
+
+
+
